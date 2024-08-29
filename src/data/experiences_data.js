46df30160385240fef987d1e2853ffb5d588f79d
atom017 <khaingjar@gmail.com>
2024-08-29 22:53:58 +0630

    [update] update experiences data

--- src/data/experiences_data.js
+++ src/data/experiences_data.js
@@ -1,45 +1,32 @@
-const experiences_data  =[
+const experiences_data = [
     {
-        "id":1,
-        "position":"Junior Python Developer",
-        "organization":"Visibleone",
-        "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, corrupti aut ipsa dolores quia enim.",
-        "start_date":"2023",
-        "end_date":"2024",
+        "id": 1,
+        "position": "AI Engineer",
+        "organization": "GlobalWalkers",
+        "source": "https://www.globalwalkers.co.jp/en/mmr/",
+        "description": "Developed computer vision project using YoloV8. \n Developed OCR system using Google OCR\n Implement projects on Docker Environment ",
+        "start_date": "2023",
+        "end_date": "2024",
     },
     {
-        "id":2,
-        "position":"AI Engineer Volunteer",
-        "organization":"TeeHtwin",
-        "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, corrupti aut ipsa dolores quia enim.",
-        "description":"A passionate AI engineer",
-        "start_date":"2023",
-        "end_date":"2024",
+        "id": 2,
+        "position": "AI Engineer",
+        "organization": "TeeHtwin",
+        "source": "",
+        "description": "Developed Apartment Recommender System. \n Developed computer vision project using YoloV8 and flask backend. \n Developed Minimal Chatbot using PandasAI ",
+        "start_date": "2023",
+        "end_date": "2024",
     },
+
     {
-        "id":3,
-        "position":"Backend Developer Intern",
-        "organization":"Visibleone",
-        "description":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, corrupti aut ipsa dolores quia enim.",
-        "start_date":"2022",
-        "end_date":"2023",
+        "id": 3,
+        "position": "Junior Python Developer",
+        "organization": "Visibleone",
+        "description": "A passionate AI engineer",
+        "start_date": "2022-2023",
+        "end_date": "",
     },
-    {
-        "id":4,
-        "position":"Junior Python Developer",
-        "organization":"Visibleone",
-        "description":"A passionate AI engineer",
-        "start_date":"2022-2023",
-        "end_date":"",
-    },
-    {
-        "id":5,
-        "position":"Software Engineer Intern",
-        "organization":"Visibleone",
-        "description":"",
-        "start_date":"",
-        "end_date":"",
-    },
+
 ]
 
 export default experiences_data;
