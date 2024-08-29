d60fd4a068691e0dbba34066212e63ceb48444ee
atom017 <khaingjar@gmail.com>
2024-08-29 22:55:15 +0630

    [update] update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,42 @@
-import React, { useState, useEffect} from 'react'
-import {AiOutlineClose} from 'react-icons/ai'
-import {FaBars} from 'react-icons/fa'
-import {motion} from 'framer-motion'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineClose } from 'react-icons/ai'
+import { FaBars } from 'react-icons/fa'
+import { motion } from 'framer-motion'
 
-const Navbar = ({scrollTo}) => {
-  const [toggle,setToggle] = useState(false)
-  function openMenu(){
+const Navbar = ({ scrollTo }) => {
+  const [toggle, setToggle] = useState(false)
+  function openMenu() {
     setToggle(true)
   }
-  function closeMenu(){
+  function closeMenu() {
     setToggle(false)
   }
 
-  
 
-  const nav_items=[
-    
+
+  const nav_items = [
+
     {
-      title:'About',
-      link:'about'
-      },
-    {
-      title:'Projects',
-      link:'projects'
+      title: 'About',
+      link: 'about'
     },
     {
-      title:'Experiences',
-      link:'experience'
-      },
-      {
-        title:'Services',
-        link:'service'
-        },
-  
-      {
-        title:'Contact',
-        link:'contact'
-        },
+      title: 'Projects',
+      link: 'projects'
+    },
+    {
+      title: 'Experiences',
+      link: 'experience'
+    },
+    {
+      title: 'Services',
+      link: 'service'
+    },
+
+    {
+      title: 'Contact',
+      link: 'contact'
+    },
   ]
 
   const [activeSection, setActiveSection] = useState('');
@@ -58,7 +58,7 @@ const Navbar = ({scrollTo}) => {
 
     // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
-    
+
     // Clean up on unmount
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
@@ -66,24 +66,24 @@ const Navbar = ({scrollTo}) => {
 
   return (
     <>
-    <nav className='sm:fixed sm:top-0 sm:z-10 w-full py-4 px-5  flex items-center justify-between lg:flex-row mx-auto shadow-lg bg-white'>
-   
+      <nav className='md:fixed sm:top-0 sm:z-10 w-full py-4 px-5  flex items-center justify-between lg:flex-row mx-auto shadow-lg bg-white'>
+
         <div className='h-10 w-10'>
-          <span className='text-gray-500 font-mono'>khaig.hsu.thwe.dev@gmail.com</span>
-        {/* <img className='rounded-full shadow-lg' src={logo} alt="" /> */}
+          <span className='text-gray-500 font-mono tracking-tight'>khaig.hsu.thwe.dev@gmail.com</span>
+          {/* <img className='rounded-full shadow-lg' src={logo} alt="" /> */}
         </div>
         <div className='space-x-4 '>
 
-          <ul className='hidden sm:flex font-mono'>
-          {nav_items.map((item, index) => (
-              <li 
+          <ul className='hidden md:flex font-mono'>
+            {nav_items.map((item, index) => (
+              <li
                 key={index}
-                className={`mr-6 relative cursor-pointer text-gray-500 text-sm md:text-lg block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#6c63ff] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${activeSection === item.link ? 'after:bg-[#6c63ff]' : ''} ${activeSection === item.link ? 'font-bold' : ''}` }
+                className={`mr-6 relative cursor-pointer  text-sm md:text-lg block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#6c63ff] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${activeSection === item.link ? 'after:bg-[#6c63ff]' : ''} ${activeSection === item.link ? 'text-[#6c63ff]' : 'text-gray-500'} ${activeSection === item.link ? 'font-bold' : ''}`}
                 onClick={() => scrollTo(item.link)}>
                 {item.title}
               </li>
             ))}
-          {/* <li 
+            {/* <li 
           className="mr-6 relative cursor-pointer text-gray-500  text-sm md:text-lg  block after:block after:content-[''] after:absolute after:h-[3px] after:bg-[#6c63ff] after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center"
            onClick={() => scrollTo('about')}>
           About
@@ -113,31 +113,31 @@ const Navbar = ({scrollTo}) => {
 
           </ul>
 
-          <div className='ssm:block sm:hidden'>
+          <div className='ssm:block md:hidden'>
             {toggle ? (
-              <AiOutlineClose onClick={closeMenu} size={30} className=' cursor-pointer'/>
-            ): (
-              <FaBars onClick={openMenu} size={30} className=' cursor-pointer'/>
+              <AiOutlineClose onClick={closeMenu} size={30} className=' cursor-pointer' />
+            ) : (
+              <FaBars onClick={openMenu} size={30} className=' cursor-pointer' />
             )}
-            
-          </div>
-  
-        </div>
-        
-    </nav>
-     {/* small screens */}
-     {toggle && (
-     <motion.div  initial={{ opacity: 0 }}
-     animate={{ opacity: 1 }}
-     exit={{ opacity: 0 }}
-     transition={{ duration: 0.2 }}
-      className='flex z-2 w-full  bg-gray-700 justify-between ssm:block sm:hidden'>
-            <div  className=' flex h-screen justify-between w-full'>
-              <ul className='w-full'>
-                {nav_items.map((item,index) =>{
 
-                  return (
-                    <motion.li key={index}
+          </div>
+
+        </div>
+
+      </nav>
+      {/* small screens */}
+      {toggle && (
+        <motion.div initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className='flex z-2 w-full  bg-gray-700 justify-between ssm:block sm:hidden'>
+          <div className=' flex h-screen justify-between w-full'>
+            <ul className='w-full'>
+              {nav_items.map((item, index) => {
+
+                return (
+                  <motion.li key={index}
                     initial={{ scale: 0, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     transition={{
@@ -146,21 +146,21 @@ const Navbar = ({scrollTo}) => {
                       damping: 20,
                       delay: 0.1 + index / 10,
                     }}
-                    onClick={() =>{
+                    onClick={() => {
                       scrollTo(item.link);
                       closeMenu();
-                    } }
+                    }}
                     className=' px-4 py-3 text-white   cursor-pointer  hover:bg-slate-500 underline-offset-4  text-lg'>
-                    
+
                     {item.title}
-                    </motion.li>
-                  )
-                })}
-                
-               
-              </ul>
-            </div>
-          </motion.div>)}
+                  </motion.li>
+                )
+              })}
+
+
+            </ul>
+          </div>
+        </motion.div>)}
     </>
   )
 }
