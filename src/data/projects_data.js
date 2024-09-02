1a0f0126b791068b296ca015ad822172b8904b36
atom017 <khaingjar@gmail.com>
2024-09-02 21:52:33 +0630

    [update] updated projects data

--- src/data/projects_data.js
+++ src/data/projects_data.js
@@ -1,58 +1,58 @@
 //import {image} from "../assets/projects/"
-const projects_data  =[
+const projects_data = [
     {
-        "id":1,
-        "image":"../../../public/assets/projects/UI Image.png",
-        "name":"Burmese Alphabet Recognition System",
-        "source":"https://github.com/atom017/Burmese-Alphabet-Recognition-System",
-        "demo":"",
-        "type":"AI",
-        "tech_stack":['Python','Tensorflow']
+        "id": 1,
+        "image": "../../../public/assets/projects/UI Image.png",
+        "name": "Burmese Alphabet Recognition System",
+        "source": "https://github.com/atom017/Burmese-Alphabet-Recognition-System",
+        "demo": "",
+        "type": "AI",
+        "tech_stack": ['Python', 'Tensorflow']
     },
     {
-        "id":2,
-        "image":"https://plus.unsplash.com/premium_photo-1705583864718-71cdbb830c79?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "name":"Simple Chat App",
-        "source":"",
-        "demo":"",
-        "type":"Web",
-        "tech_stack":['React', 'Node']
+        "id": 2,
+        "image": "https://plus.unsplash.com/premium_photo-1705583864718-71cdbb830c79?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "name": "Simple Chat App",
+        "source": "https://github.com/atom017/simple-chat-app-client",
+        "demo": "https://nf-simple-chat-app.netlify.app/",
+        "type": "Web",
+        "tech_stack": ['React', 'Node']
     },
     {
-        "id":3,
-        "image":"https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "name":"Heart Disease Prediction",
-        "source":"",
-        "demo":"",
-        "type":"AI",
-        "tech_stack":['React', 'Node']
+        "id": 3,
+        "image": "https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "Heart Disease Prediction",
+        "source": "",
+        "demo": "",
+        "type": "AI",
+        "tech_stack": ['React', 'Node']
     },
     {
-        "id":4,
-        "image":"https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "name":"Alpaca Image Generator",
-        "source":"",
-        "demo":"",
-        "type":"Web",
-        "tech_stack":['React', 'Node']
+        "id": 4,
+        "image": "https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "FreshBites",
+        "source": "",
+        "demo": "https://freshbites.onrender.com",
+        "type": "Web",
+        "tech_stack": ['React', 'Node']
     },
     {
-        "id":5,
-        "image":"https://images.pexels.com/photos/1006293/pexels-photo-1006293.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "name":"Blog Website",
-        "source":"",
-        "demo":"",
-        "type":"Web",
-        "tech_stack":['React', 'Node']
+        "id": 5,
+        "image": "https://images.pexels.com/photos/1006293/pexels-photo-1006293.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "Blog Website",
+        "source": "https://github.com/atom017/Blog-Website",
+        "demo": "https://blog-website-react.netlify.app/",
+        "type": "Web",
+        "tech_stack": ['React', 'Node']
     },
     {
-        "id":6,
-        "image":"https://images.pexels.com/photos/434337/pexels-photo-434337.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "name":"Alpaca Image Generator",
-        "source":"",
-        "demo":"",
-        "type":"Web",
-        "tech_stack":['React', 'Node']
+        "id": 6,
+        "image": "https://images.pexels.com/photos/434337/pexels-photo-434337.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "Alpaca Image Generator",
+        "source": "https://github.com/atom017/Alpaca-Image-Generator",
+        "demo": "https://alpaca-codementorchallenge.netlify.app/",
+        "type": "Web",
+        "tech_stack": ['React', 'Node']
     },
 ]
 
