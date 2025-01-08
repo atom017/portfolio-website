185ed3e838e930589aa937af4993c557d1b32964
atom017 <khaingjar@gmail.com>
2025-01-08 22:48:00 +0630

    [add] add projects data

---
+++ data/projects_data.js
@@ -0,0 +1,63 @@
+//import {image} from "../assets/projects/"
+const projects_data = [
+    {
+        "id": 1,
+        "image": "https://images.pexels.com/photos/238118/pexels-photo-238118.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "E-Commerce Project",
+        "source": "https://github.com/atom017/e-commerce-frontend",
+        "demo": "https://my-e-shop-frontend.onrender.com",
+        "type": "Web",
+        "tags": ['React.js', 'Node.js', 'MongoDB']
+    },
+    {
+        "id": 1,
+        "image": "/assets/projects/canvas.png",
+        "name": "Burmese Alphabet Recognition System",
+        "source": "https://github.com/atom017/Burmese-Alphabet-Recognition-System",
+        "demo": "https://atom017-burmese-alphabet-recognition-system-main-page-65ovyw.streamlit.app/",
+        "type": "AI",
+        "description": "The System recognizes handwritten Burmese alphabets and give text as output",
+        "tags": ['Tensorflow', 'CNN', 'Scikit-learn', 'Streamlit']
+    },
+    {
+        "id": 2,
+        "image": "https://images.pexels.com/photos/434337/pexels-photo-434337.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "Simple Chat App",
+        "source": "https://github.com/atom017/simple-chat-app-client",
+        "demo": "https://nf-simple-chat-app.netlify.app/",
+        "type": "Web",
+        "description": "Simple chat appliction to chat through rooms",
+        "tags": ['React.js', 'Node.js', 'Socketio']
+    },
+    {
+        "id": 3,
+        "image": "https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "name": "Heart Disease Prediction",
+        "source": "",
+        "demo": "",
+        "type": "AI",
+        "tags": ["CNN", "Scikit-learn", "Streamlit"]
+    },
+
+    {
+        "id": 5,
+        "image": "/assets/projects/blog.png",
+        "name": "Blog Website",
+        "source": "https://github.com/atom017/Blog-Website",
+        "demo": "https://blog-website-react.netlify.app/",
+        "type": "Web",
+        "description": "A simple blog website",
+        "tags": ['React.js', 'Firebase']
+    },
+    {
+        "id": 6,
+        "image": "/assets/projects/alpaca.png",
+        "name": "Alpaca Image Generator",
+        "source": "https://github.com/atom017/Alpaca-Image-Generator",
+        "demo": "https://alpaca-codementorchallenge.netlify.app/",
+        "type": "Web",
+        "tags": ['Reac.js']
+    },
+]
+
+export default projects_data;
