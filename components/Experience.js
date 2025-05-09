5b035b3ae236adacf36e859530287c94a38ca22d
khainghsuthwe <khaingjar@gmail.com>
2025-05-09 10:59:22 +0630

    [update] updated experience section

--- components/Experience.js
+++ components/Experience.js
@@ -60,6 +60,27 @@ const Experience = () => {
               </ul>
             </div>
           </motion.div>
+
+          {/* Experience 3 */}
+          <motion.div
+            className="flex items-center space-x-4 bg-gray-700 p-6 rounded-lg shadow-lg relative z-10"
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="text-[#8c6fc4] text-4xl relative z-10">
+              <FaBriefcase />
+            </div>
+            <div>
+              <h3 className="text-xl font-semibold">AI Engineer at TeeHtwin</h3>
+              <p className="text-sm text-gray-400">2023 Feb - 2025 Jan</p>
+              <ul className="mt-2 list-disc pl-5 text-gray-400">
+                <li>Developed apart recommender system for a realestate website which shows apartments in US, Texas.</li>
+                <li>Developed a chatbot using Flask, langchain and Reactjs for an education website. </li>
+                <li>Worked on backend, frontend and AI related tasks.</li>
+              </ul>
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
