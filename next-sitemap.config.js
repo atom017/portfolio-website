e8c9c5b79b34bc9803c1a776275f42dcb65cb19d
khainghsuthwe <khaingjar@gmail.com>
2025-05-15 12:33:53 +0630

    [add] added next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://khainghsuthwe.vercel.app", // Replace with your Vercel domain
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/private/*"], // Exclude private routes if any
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+};
